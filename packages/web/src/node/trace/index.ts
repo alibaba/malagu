091b639db8f9265da27773f3d46ec3d7bb9b3890
linxiaowu66 <linguang661990@126.com>
2020-02-07 13:53:33 +0800

    feat(web:trace): fix file name

--- packages/web/src/node/trace/index.ts
+++ packages/web/src/node/trace/index.ts
@@ -1,4 +1,4 @@
 export * from './trace-middleware';
 export * from './trace-protocol';
-export * from './trace-resolver-protocol';
-export * from './trace-resolver';
+export * from './trace-id-resolver-protocol';
+export * from './trace-id-resolver';
